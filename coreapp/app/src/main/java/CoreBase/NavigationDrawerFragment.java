9558da12e931b6728c6422e1764ef9313000b821
Alan Costa <alancosta@Alans-MacBook-Pro.local>
2016-05-22 21:21:52 -0300

    start of navigation drawer development

--- coreapp/app/src/main/java/CoreBase/NavigationDrawerFragment.java
+++ coreapp/app/src/main/java/CoreBase/NavigationDrawerFragment.java
@@ -263,7 +263,7 @@ public class NavigationDrawerFragment extends Fragment {
                     mUserLearnedDrawer = true;
                 }
 
-                getActivity().supportInvalidateOptionsMenu();
+//                getActivity().supportInvalidateOptionsMenu();
             }
         };
 
